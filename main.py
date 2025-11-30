0dca461a542c1c41b87a0b0e080810ea04ce10f3
Jack-McGinley <120057331+Jack-McGinley@users.noreply.github.com>
2025-11-30 10:03:40 -0500

    Added new files and updated files via upload
    #Added return statement at end of configure method in Data_Configuration class to pass output to plotandanalyze class.
    #Added plotandAnalyze class to main method 
    #Add '**kwargs' attribute to load_data method of SolarPowerModel class to allow users to pass solar power module parameters
    #Created PlotAnalysis class to create a power vs time graph to determine the months that generate the most power
    #Included plot figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Import Classes from other modules
 from Data_Configuration import Data_Config as DC
+from PlotAnalysis import PlotAnalysis as PA
 
 file_path = "C:\\Users\\jackp\\Documents\\Python\\EE551\\Project\\nsrdb_2024.csv"
 
@@ -11,17 +12,23 @@ def main():
     while True:
         #User can opt to use default parameters or input their own
         if input('Use default parameters? (y/n): ') == 'y':
-            config = DC(file_path)    
+            config = DC(file_path) #Use default parameters in Data_Config class 
         else:
             A = float(input('Area: '))
             E = float(input('Efficiency: '))
             T = float(input('Temperature Coefficient: '))
             N = float(input('NOCT: '))
 
-            config = DC(file_path, A, E, T, N)
+            config = DC(file_path, A, E, T, N) #Pass attributes to Data_Config class
         #Return configured CSV file, including power output based on user parameters
         csv_file = config.configure()
+        #Create PlotAnalysis object with configured CSV file
+        plotter = PA(csv_file)
+        #Save figure of plot analysis
+        plotter.plot_power_vs_time()
+        #Notify user of saved file and configured file
         print(f'Configured data saved to {csv_file}')
+        print('Plot saved as solar_power_output.png')
         #Ask user if they would like to try new parameters
         print('Would you like to try new parameters? (y/n): ')
         choice = input()
